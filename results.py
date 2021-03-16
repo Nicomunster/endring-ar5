64ad68fb4cca5ca705bc1d32092d9649bd6fb212
Nicomunster <33160291+Nicomunster@users.noreply.github.com>
2021-03-16 08:45:38 +0100

    La til Sør-Odal og Eidskog

--- results.py
+++ results.py
@@ -9,16 +9,16 @@ import numpy as np
 import pandas as pd
 import evaluate as ev
 
-fra_excel = True
+fra_excel = False
 #%% Laste inn filer
 if not fra_excel:
-    # Nes
+    print("Nes")
     nes_df = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3034_Nes_2010_nobyg_4cities_300epochs\resultater\nes_pred_ar5_endring.csv", low_memory=False)
     nes_tmyr = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3034_Nes_2010_nobyg_4cities_300epochs\resultater\nes_ruter_med_tresatt_myr.csv")    
     nes_results = ev.evaluate_predictions(nes_df, 100, nes_tmyr['Id'], None, metrics="many", path=r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3034_Nes_2010_nobyg_4cities_300epochs\resultater\nes_score.xlsx")
     nes_results_for = ev.evaluate_artype_for(nes_df, nes_tmyr['Id'], r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3034_Nes_2010_nobyg_4cities_300epochs\resultater\nes_score_artype_for.xlsx")
     nes_results_etter = ev.evaluate_artype_etter(nes_df, nes_tmyr['Id'], r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3034_Nes_2010_nobyg_4cities_300epochs\resultater\nes_score_artype_etter.xlsx")
-    # Nord-Aurdal
+    print("Nord-Aurdal")
     nordaurdal_df = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3451_Nord_Aurdal_2010_nobyg_4cities_300epochs\resultater\nordaurdal_pred_ar5_endring.csv", low_memory=False)
     nordaurdal_tmyr = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3451_Nord_Aurdal_2010_nobyg_4cities_300epochs\resultater\nordaurdal_ruter_med_tresatt_myr.csv")
     nordaurdal_results = ev.evaluate_predictions(nordaurdal_df, 100, nordaurdal_tmyr['Id'], None, metrics="many", path=r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3451_Nord_Aurdal_2010_nobyg_4cities_300epochs\resultater\nordaurdal_score.xlsx")
@@ -26,30 +26,41 @@ if not fra_excel:
     nordaurdal_results_etter = ev.evaluate_artype_etter(nordaurdal_df, nordaurdal_tmyr['Id'], r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3451_Nord_Aurdal_2010_nobyg_4cities_300epochs\resultater\nordaurdal_score_artype_etter.xlsx")
     ullensaker_df = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3033_Ullensaker_2010_nobyg_4cities_300epochs\resultater\ullensaker_pred_ar5_endring.csv", low_memory=False)
     ullensaker_tmyr = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3033_Ullensaker_2010_nobyg_4cities_300epochs\resultater\ullensaker_ruter_med_tresatt_myr.csv")
-    # Ullensaker
+    print("Ullensaker")
     ullensaker_df = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3033_Ullensaker_2010_nobyg_4cities_300epochs\resultater\ullensaker_pred_ar5_endring.csv", low_memory=False)
     ullensaker_tmyr = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3033_Ullensaker_2010_nobyg_4cities_300epochs\resultater\ullensaker_ruter_med_tresatt_myr.csv")
     ullensaker_results = ev.evaluate_predictions(ullensaker_df, 100, ullensaker_tmyr['Id'], None, metrics="many", path=r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3033_Ullensaker_2010_nobyg_4cities_300epochs\resultater\ullensaker_score.xlsx")
     ullensaker_results_for = ev.evaluate_artype_for(ullensaker_df, ullensaker_tmyr['Id'], r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3033_Ullensaker_2010_nobyg_4cities_300epochs\resultater\ullensaker_score_artype_for.xlsx")
     ullensaker_results_etter = ev.evaluate_artype_etter(ullensaker_df, ullensaker_tmyr['Id'], r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3033_Ullensaker_2010_nobyg_4cities_300epochs\resultater\ullensaker_score_artype_etter.xlsx")
-    # Etnedal
+    print("Etnedal")
     etnedal_df = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3450_Etnedal_2010_nobyg_4cities_300epochs\Resultater\etnedal_pred_ar5_endring.csv", low_memory=False)
     etnedal_tmyr = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3450_Etnedal_2010_nobyg_4cities_300epochs\Resultater\etnedal_ruter_med_tresatt_myr.csv")    
     etnedal_results = ev.evaluate_predictions(etnedal_df, 100, etnedal_tmyr['Id'], None, metrics="many", path=r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3450_Etnedal_2010_nobyg_4cities_300epochs\resultater\etnedal_score.xlsx")
     etnedal_results_for = ev.evaluate_artype_for(etnedal_df, etnedal_tmyr['Id'], r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3450_Etnedal_2010_nobyg_4cities_300epochs\resultater\etnedal_score_artype_for.xlsx")
     etnedal_results_etter = ev.evaluate_artype_etter(etnedal_df, etnedal_tmyr['Id'], r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3450_Etnedal_2010_nobyg_4cities_300epochs\resultater\etnedal_score_artype_etter.xlsx")
-    # Gjerdrum
+    print("Gjerdrum")
     gjerdrum_df = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3032_Gjerdrum_2010_nobyg_4cities_300epochs\Resultater\gjerdrum_pred_ar5_endring.csv", low_memory=False)
     gjerdrum_tmyr = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3032_Gjerdrum_2010_nobyg_4cities_300epochs\Resultater\gjerdrum_ruter_med_tresatt_myr.csv")
     gjerdrum_results = ev.evaluate_predictions(gjerdrum_df, 100, gjerdrum_tmyr['Id'], None, metrics="many", path = r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3032_Gjerdrum_2010_nobyg_4cities_300epochs\Resultater\gjerdrum_score.xlsx")
     gjerdrum_results_for = ev.evaluate_artype_for(gjerdrum_df, gjerdrum_tmyr['Id'], r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3032_Gjerdrum_2010_nobyg_4cities_300epochs\Resultater\gjerdrum_score_artype_for.xlsx")
     gjerdrum_results_etter = ev.evaluate_artype_etter(gjerdrum_df, gjerdrum_tmyr['Id'], r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3032_Gjerdrum_2010_nobyg_4cities_300epochs\Resultater\gjerdrum_score_artype_etter.xlsx")
-    
-    # Samlet
+    print("Sør-Odal")
+    sorodal_df = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3415_Sør-Odal_2010_nobyg_4cities_300epochs\Resultater\sorodal_pred_ar5_endring.csv", low_memory=False)
+    sorodal_tmyr = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3415_Sør-Odal_2010_nobyg_4cities_300epochs\Resultater\sorodal_ruter_med_tresatt_myr.csv")
+    sorodal_results = ev.evaluate_predictions(sorodal_df, 100, sorodal_tmyr['Id'], None, metrics="many", path=r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3415_Sør-Odal_2010_nobyg_4cities_300epochs\Resultater\sorodal_score.xlsx")
+    sorodal_results_for = ev.evaluate_artype_for(sorodal_df, sorodal_tmyr['Id'], r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3415_Sør-Odal_2010_nobyg_4cities_300epochs\Resultater\sorodal_score_artype_for.xlsx")
+    sorodal_results_etter = ev.evaluate_artype_etter(sorodal_df, sorodal_tmyr['Id'], r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3415_Sør-Odal_2010_nobyg_4cities_300epochs\Resultater\sorodal_score_artype_etter.xlsx")
+    print("Eidskog")
+    eidskog_df = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3416_Eidskog_2010_nobyg_4cities_300epochs\Resultater\eidskog_pred_ar5_endring.csv", low_memory=False)
+    eidskog_tmyr = pd.read_csv(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3416_Eidskog_2010_nobyg_4cities_300epochs\Resultater\eidskog_ruter_med_tresatt_myr.csv")
+    eidskog_results = ev.evaluate_predictions(eidskog_df, 100, eidskog_tmyr['Id'], None, metrics="many", path=r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3416_Eidskog_2010_nobyg_4cities_300epochs\Resultater\eidskog_score.xlsx")
+    eidskog_results_for = ev.evaluate_artype_for(eidskog_df, eidskog_tmyr['Id'], r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3416_Eidskog_2010_nobyg_4cities_300epochs\Resultater\eidskog_score_artype_for.xlsx")
+    eidskog_results_etter = ev.evaluate_artype_etter(eidskog_df, eidskog_tmyr['Id'], r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3416_Eidskog_2010_nobyg_4cities_300epochs\Resultater\eidskog_score_artype_etter.xlsx")
+    print("Samlet")
     # Laster inn dataframes og tresatt myr
-    dfs = [nes_df, nordaurdal_df, ullensaker_df, etnedal_df, gjerdrum_df]
-    tmyrs = [nes_tmyr, nordaurdal_tmyr, ullensaker_tmyr, etnedal_tmyr, gjerdrum_tmyr]
-    kommuner = ["Nes", "Nord-Aurdal", "Ullensaker", "Etnedal", "Gjerdrum"]
+    dfs = [nes_df, nordaurdal_df, ullensaker_df, etnedal_df, gjerdrum_df, sorodal_df]
+    tmyrs = [nes_tmyr, nordaurdal_tmyr, ullensaker_tmyr, etnedal_tmyr, gjerdrum_tmyr, sorodal_tmyr]
+    kommuner = ["Nes", "Nord-Aurdal", "Ullensaker", "Etnedal", "Gjerdrum", "Sør-Odal", "Eidskog"]
     
     # Prediksjoner
     preds_stack = []
@@ -152,12 +163,7 @@ if not fra_excel:
                 results.to_excel(writer, str(int(artype)))
                 # Lagrer resultater for artypen i dictionary
                 samlet_results_etter[str(artype)] = results
-    
-    results_dict = {"Nes": {"Resultater": nes_results, "Etter": nes_results_etter, "Før": nes_results_for},
-                    "Nord-Aurdal": {"Resultater": nordaurdal_results, "Etter": nordaurdal_results_etter, "Før": nordaurdal_results_for},
-                    "Ullensaker": {"Resultater": ullensaker_results, "Etter": ullensaker_results_etter, "Før": ullensaker_results_for},
-                    "Etnedal": {"Resultater": etnedal_results, "Etter": etnedal_results_etter, "Før": etnedal_results_for},
-                    "Gjerdrum": {"Resultater": gjerdrum_results, "Etter": gjerdrum_results_etter, "Før": gjerdrum_results_for}}
+
 
 if fra_excel:
     # Nes
@@ -180,17 +186,27 @@ if fra_excel:
     gjerdrum_results = pd.read_excel(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3032_Gjerdrum_2010_nobyg_4cities_300epochs\Resultater\gjerdrum_score.xlsx")
     gjerdrum_results_for = pd.read_excel(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3032_Gjerdrum_2010_nobyg_4cities_300epochs\Resultater\gjerdrum_score_artype_for.xlsx", None)
     gjerdrum_results_etter = pd.read_excel(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3032_Gjerdrum_2010_nobyg_4cities_300epochs\Resultater\gjerdrum_score_artype_etter.xlsx", None)
+    # Sør-Odal
+    sorodal_results = pd.read_excel(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3415_Sør-Odal_2010_nobyg_4cities_300epochs\Resultater\sorodal_score.xlsx")
+    sorodal_results_for = pd.read_excel(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3415_Sør-Odal_2010_nobyg_4cities_300epochs\Resultater\sorodal_score_artype_for.xlsx")
+    sorodal_results_etter = pd.read_excel(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3415_Sør-Odal_2010_nobyg_4cities_300epochs\Resultater\sorodal_score_artype_etter.xlsx")
+    # Eidskog
+    eidskog_results = pd.read_excel(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3416_Eidskog_2010_nobyg_4cities_300epochs\Resultater\eidskog_score.xlsx")
+    eidskog_results_for = pd.read_excel(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3416_Eidskog_2010_nobyg_4cities_300epochs\Resultater\eidskog_score_artype_for.xlsx")
+    eidskog_results_etter = pd.read_excel(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\3416_Eidskog_2010_nobyg_4cities_300epochs\Resultater\eidskog_score_artype_etter.xlsx")
     # Samlet
     samlet_results = pd.read_excel(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\Samlet\Resultater\samlet_score.xlsx")
     samlet_results_for = pd.read_excel(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\Samlet\Resultater\samlet_score_artype_for.xlsx", None)
     samlet_results_etter = pd.read_excel(r"C:\Users\nicol\Documents\Masteroppgave\Februarprediksjoner\Samlet\Resultater\samlet_score_artype_etter.xlsx", None)
     
-    results_dict = {"Nes": {"Resultater": nes_results, "Etter": nes_results_etter, "Før": nes_results_for},
-                    "Nord-Aurdal": {"Resultater": nordaurdal_results, "Etter": nordaurdal_results_etter, "Før": nordaurdal_results_for},
-                    "Ullensaker": {"Resultater": ullensaker_results, "Etter": ullensaker_results_etter, "Før": ullensaker_results_for},
-                    "Etnedal": {"Resultater": etnedal_results, "Etter": etnedal_results_etter, "Før": etnedal_results_for},
-                    "Gjerdrum": {"Resultater": gjerdrum_results, "Etter": gjerdrum_results_etter, "Før": gjerdrum_results_for},
-                    "Samlet": {"Resultater": samlet_results, "Etter": samlet_results_etter, "Før": samlet_results_for}}
+results_dict = {"Nes": {"Resultater": nes_results, "Etter": nes_results_etter, "Før": nes_results_for},
+                "Nord-Aurdal": {"Resultater": nordaurdal_results, "Etter": nordaurdal_results_etter, "Før": nordaurdal_results_for},
+                "Ullensaker": {"Resultater": ullensaker_results, "Etter": ullensaker_results_etter, "Før": ullensaker_results_for},
+                "Etnedal": {"Resultater": etnedal_results, "Etter": etnedal_results_etter, "Før": etnedal_results_for},
+                "Gjerdrum": {"Resultater": gjerdrum_results, "Etter": gjerdrum_results_etter, "Før": gjerdrum_results_for},
+                "Sør-Odal": {"Resultater": sorodal_results, "Etter": sorodal_results_etter, "Før": sorodal_results_for},
+                "Eidskog": {"Resultater": eidskog_results, "Etter": eidskog_results_etter, "Før": eidskog_results_for},
+                "Samlet": {"Resultater": samlet_results, "Etter": samlet_results_etter, "Før": samlet_results_for}}
 
 #%% Plotting
 
@@ -216,6 +232,6 @@ def plot_kommuner(kommuner, tid, gridcode, metric):
         if "Etter" in tid:
             ev.artype_barplot(results_etter, results, gridcode, metric, title=kommune+" etter endringer")
         
-#kommuner = ["Nes", "Nord-Aurdal", "Ullensaker", "Etnedal", "Gjerdrum", "Samlet"]
-kommuner = ["Nes", "Ullensaker", "Gjerdrum"]
+kommuner = ["Nes", "Nord-Aurdal", "Ullensaker", "Etnedal", "Gjerdrum", "Sør-Odal", "Eidskog", "Samlet"]
+#kommuner = ["Nes", "Ullensaker", "Gjerdrum"]
 plot_kommuner(kommuner, "Før", 50, "MCC")
